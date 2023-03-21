b57b25b6bb3ebba18c22ca95635f75a9de5299e2
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-03-21 20:32:10 -0300

    Add unique=True

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Links(models.Model):
     link_original = models.URLField()
-    link_encurtado = models.CharField(max_length=8)
+    link_encurtado = models.CharField(max_length=8, unique=True)
 
     def __str__(self):
         return self.link_encurtado
