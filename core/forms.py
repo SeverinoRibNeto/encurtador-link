32a14b15ccaa5d74a4573bd6b74f8855cf96c7ee
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-03-21 20:31:28 -0300

    Add css class into fields form

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,14 @@ from django import forms
 from .models import Links
 
 
-class FormLinks(forms.Form):
+class FormLinks(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(FormLinks, self).__init__(*args, **kwargs)
+        self.required_css_class = 'form-control'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Links
         fields = "__all__"
