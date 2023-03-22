a372093833b55266d4972386b80658f0e900a310
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-03-22 20:47:18 -0300

    Add url path: encurtar_link and <str:link>

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("encurtar_link/", views.encurtar_link, name="encurtar_link"),
+    path("<str:link>", views.redirecionador_de_link,
+         name="redirecionador_de_link"),
 ]
