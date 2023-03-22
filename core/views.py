64441be47f8fa7f0d06ade9d9202e78681350808
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-03-22 20:48:30 -0300

    Implements views encurtar_link

--- core/views.py
+++ core/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .forms import FormLinks
+from .models import Links
 
 
 def home(request):
     form = FormLinks()
-    return render(request, 'home.html', {'form': form})
+    status = request.GET.get('status')
+    return render(request, 'home.html', {'form': form, 'status': status})
+
+
+def encurtar_link(request):
+    form = FormLinks(request.POST)
+    link_encurtado = form.data['link_encurtado']
+    links = Links.objects.filter(link_encurtado=link_encurtado)
+    if (len(links) > 0):
+        return redirect('/?status=1')
+
+    if (form.is_valid()):
+        try:
+            form.save()
+            return HttpResponse(f"Seu link foi criado com sucesso! Para acessar,acesse: https://127.0.0.1:8000/{link_encurtado}")
+        except Exception as e:
+            return HttpResponse("Erro interno do sistema: "+e)
+
+
+def redirecionador_de_link(request, link_recebido):
+    link = Links.objects.get(link_encurtado=link_recebido)
+    if len(link) == 0:
+        return redirect("/")
+
+    return HttpResponse(link)
