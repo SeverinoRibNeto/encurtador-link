db7ff742410e08a5574bcbc26005f1bed3712430
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-04-01 13:56:19 -0300

    alter name of link to link_recebido

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("encurtar_link/", views.encurtar_link, name="encurtar_link"),
-    path("<str:link>", views.redirecionador_de_link,
+    path("<str:link_recebido>", views.redirecionador_de_link,
          name="redirecionador_de_link"),
 ]
