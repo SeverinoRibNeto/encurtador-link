8c7460f31e759915fc7e3f2d7c717e7ddd96f60e
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-04-01 13:56:36 -0300

    create a view to redirect

--- core/views.py
+++ core/views.py
@@ -15,19 +15,19 @@ def encurtar_link(request):
     link_encurtado = form.data['link_encurtado']
     links = Links.objects.filter(link_encurtado=link_encurtado)
     if (len(links) > 0):
-        return redirect('/?status=1')
+        return redirect('/?status=1')  # Status 1 = Link já existe
 
     if (form.is_valid()):
         try:
             form.save()
-            return HttpResponse(f"Seu link foi criado com sucesso! Para acessar,acesse: https://127.0.0.1:8000/{link_encurtado}")
+            return HttpResponse(f"Seu link foi criado com sucesso! Para acessar,acesse: http://127.0.0.1:8000/{link_encurtado}")
         except Exception as e:
             return HttpResponse("Erro interno do sistema: "+e)
 
 
 def redirecionador_de_link(request, link_recebido):
-    link = Links.objects.get(link_encurtado=link_recebido)
+    link = Links.objects.filter(link_encurtado=link_recebido)
     if len(link) == 0:
-        return redirect("/")
-
-    return HttpResponse(link)
+        return redirect("/?status=2")  # Link digitado não existe
+    link_original = link[0].link_original
+    return redirect(link_original)
